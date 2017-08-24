a2bbb49e761cbe1bec05d52501b2c8fa0ed68fe1
Menezes Elvis Edward <menezeselvis7@gmail.com>
2017-08-24 15:51:43 +0530

    add local stoarge and save the list

--- popup.js
+++ popup.js
@@ -1,12 +1,14 @@
 
 
 var currUrl;
+var t = 1;
+var i= 1;
+
+
 chrome.tabs.query({active: true,lastFocusedWindow: true}, function(tabs) {
 		   var tab = tabs[0];
 		   currUrl = tab.url;
 });
-var t = 1;
-var i= 1;
  function youtubeMain(url){
 	videoid = url.match(/(?:https?:\/{2})?(?:w{3}\.)?youtu(?:be)?\.(?:com|be)(?:\/watch\?v=|\/)([^\s&]+)/);
 	thumbnail = "https://img.youtube.com/vi/" + videoid[1] + "/hqdefault.jpg";
@@ -33,6 +35,24 @@ var i= 1;
 	image.className = "playlist";
 	image.src = thumbnail;
 	imageParent.appendChild(image);
+	
+	var remove = document.createElement("button");
+	var buttonParent = document.getElementById("l" + t);
+	remove.id = "r"+t;
+	remove.className = "removeButtons";
+	remove.onclick = function () {
+    this.parentElement.parentElement.removeChild(buttonParent);
+	 setTimeout(function() {
+		this.parentElement.className = this.parentElement.className + " removeshow";
+	}, 2000);
+	};
+	buttonParent.appendChild(remove);
+	
+	var icon = document.createElement("i");
+	var iconParent = document.getElementById("r"+t);
+	icon.className="fa fa-trash";
+	icon.id= "i"+t;
+	iconParent.appendChild(icon);
 	t = t+1;
  }
  
@@ -44,6 +64,12 @@ var i= 1;
      chrome.tabs.update({url: $(this).attr('href')});
      return false;
    });
+   
+    document.getElementByclassName('removebuttons').onclick = function(){
+        this.parentNode.parentNode.parentNode
+        .removeChild(this.parentNode.parentNode);
+        return false;
+    };
 });
 
 
