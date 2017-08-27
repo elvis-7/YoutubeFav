4f0ac368cf5454d9343ac23af2116b3fdf4d02bd
Menezes Elvis Edward <menezeselvis7@gmail.com>
2017-08-27 17:40:07 +0530

    update remove from local storage

--- popup.js
+++ popup.js
@@ -4,16 +4,17 @@ var currUrl;
 var t = 1;
 var i= 1;
 
-
 chrome.tabs.query({active: true,lastFocusedWindow: true}, function(tabs) {
 		   var tab = tabs[0];
 		   currUrl = tab.url;
 });
+
+
+
+
  function youtubeMain(url){
 	videoid = url.match(/(?:https?:\/{2})?(?:w{3}\.)?youtu(?:be)?\.(?:com|be)(?:\/watch\?v=|\/)([^\s&]+)/);
 	thumbnail = "https://img.youtube.com/vi/" + videoid[1] + "/hqdefault.jpg";
-	
-	
 	
 	var item = document.createElement('div');
 	var itemParent = document.getElementById("list");
@@ -54,11 +55,30 @@ chrome.tabs.query({active: true,lastFocusedWindow: true}, function(tabs) {
 	icon.id= "i"+t;
 	iconParent.appendChild(icon);
 	t = t+1;
+	return false;
  }
- 
+ function loadChanges(){
+	chrome.storage.local.get('urlList',function(items){
+		for(var i = 0; i<items.urlList.length;i++){
+			var ret =items.urlList[i];
+			youtubeMain(ret.currUrl);
+	    }
+	});
+	return false;
+}
+
  $(document).ready(function(){
+	loadChanges();
     $("#addtolist").click(function(){
-		   youtubeMain(currUrl);
+		chrome.storage.local.get({ urlList: []},function (items){
+				items.urlList.push({currUrl});
+				chrome.storage.local.set({ urlList: items.urlList }, function () {
+				if (chrome.runtime.lastError) {
+                alert('ERROR: ' + chrome.runtime.lastError.message);
+				}
+            });
+		});
+		youtubeMain(currUrl);
     });
 	 $('body').on('click', 'a', function(){
      chrome.tabs.update({url: $(this).attr('href')});
